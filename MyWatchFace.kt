922ee75c272477b499ec5d2101a7df5f8aeb4642
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-08-11 03:15:34 -0700

    Update MyWatchFace.kt

--- MyWatchFace.kt
+++ MyWatchFace.kt
@@ -1381,49 +1381,6 @@ class MyWatchFace : CanvasWatchFaceService() {
                 mUpdateTimeHandler.sendEmptyMessageDelayed(MSG_UPDATE_TIME, delayMs)
             }
         }
-        private fun getHoroscope(): String {
-
-            val sdf = SimpleDateFormat("EEE")
-            val sdf1 = SimpleDateFormat("EEEE")
-            val sdf2 = SimpleDateFormat("MMMM")
-            val sdf3 = SimpleDateFormat("d")
-            val sdf4 = SimpleDateFormat("yyyy")
-            val sdf5 = SimpleDateFormat("MMMM d yyyy")
-            val d = Date()
-            val dayOfTheWeek: String = sdf.format(d)
-            val dayOfTheWeekLong: String = sdf1.format(d)
-            val monthOfYear: String = sdf2.format(d)
-            val dayOfMonth: String = sdf3.format(d)
-            val year4digits: String = sdf4.format(d)
-            val fullDateSpaces: String = sdf5.format(d)
-
-            val horoscopeString = when(monthOfYear){
-                "January" -> if(Integer.parseInt(dayOfMonth) in 1..19){ "Capricorn" }
-                else {"Aquarius" }
-                "February" ->  if(Integer.parseInt(dayOfMonth) in 1..18 ){"Aquarius"}
-                else {"Pisces"}
-                "March" -> if(Integer.parseInt(dayOfMonth) in 1..20 ){"Pisces"}
-                else{ "Aries"}
-                "April" -> if(Integer.parseInt(dayOfMonth) in 1..19 ){"Aries"}
-                else {"Taurus"}
-                "May" -> {"Taurus"}
-                "June" -> if(Integer.parseInt(dayOfMonth) in 1..20 ){"Gemini"}
-                else{"Cancer"}
-                "July" -> if(Integer.parseInt(dayOfMonth) in 1..22) {"Cancer"}
-                else {"Leo"}
-                "August" ->if(Integer.parseInt(dayOfMonth) in 1..22){ "Leo"}
-                else {"Virgo"}
-                "September" -> if(Integer.parseInt(dayOfMonth) in 1..22) {"Virgo"}
-               else{"Libra"}
-                "October" -> if(Integer.parseInt(dayOfMonth) in 1..22) {"Libra"}
-                else {"Scorpio"}
-                "November" ->if(Integer.parseInt(dayOfMonth) in 1..21) { "Scorpio"}
-                else {"Sagittarius"}
-                "December" -> if(Integer.parseInt(dayOfMonth) in 1..21) { "Sagittarius"}
-                else{ "Capricorn"}
-                else -> "Cancer" }
-            return horoscopeString
-        }
-
+        
     }
 }
