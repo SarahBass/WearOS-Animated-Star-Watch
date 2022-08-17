698cda26bb87f894d0d9ac028912eff7622183a7
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-08-17 15:25:33 -0700

    Create MyWatchFaceUtils.kt

---
+++ app/src/main/java/com/academy/testwatch3/MyWatchFaceUtils.kt
@@ -0,0 +1,10 @@
+package com.academy.testwatch3
+
+import androidx.annotation.VisibleForTesting
+import java.util.*
+
+class MyWatchFaceUtils {
+    fun getHolidayBackgroundDrawable(date: Date): Int {
+        return R.drawable.cincodemayo
+    }
+}
