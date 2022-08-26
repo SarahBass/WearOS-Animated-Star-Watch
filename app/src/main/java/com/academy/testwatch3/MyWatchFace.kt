f02da8d89e5447e87e483fac97787aa3763d312f
Sarah Bass <69780815+SarahBass@users.noreply.github.com>
2022-08-25 18:33:37 -0700

    Update MyWatchFace.kt

--- app/src/main/java/com/academy/testwatch3/MyWatchFace.kt
+++ app/src/main/java/com/academy/testwatch3/MyWatchFace.kt
@@ -19,7 +19,7 @@ import androidx.palette.graphics.Palette
 import java.lang.ref.WeakReference
 import java.text.SimpleDateFormat
 import java.util.*
-
+import kotlin.math.abs
 
 
 /**
@@ -469,6 +469,218 @@ class MyWatchFace : CanvasWatchFaceService() {
         }
 
 
+        private fun getMoonPhase(): String {
+            val d = Date()
+            val sdf1 = SimpleDateFormat("d")
+            val dayOfMonth: String = sdf1.format(d)
+            val LUNAR_MONTH = 29.530588853;
+            val newMoondifference = abs((Integer.parseInt(dayOfMonth)) - (Integer.parseInt(getnewMoonDate())))
+            val fullMoondifference = abs((Integer.parseInt(dayOfMonth)) - (Integer.parseInt(getFullMoonDate())))
+            val moonPercent : Double = newMoondifference / LUNAR_MONTH
+
+            val moonString : String =
+                if (fullMoondifference == 0 ){"Full Moon"}
+                else if (newMoondifference == 0 ){"New Moon"}
+                else if (Integer.parseInt(dayOfMonth) > (Integer.parseInt(getFullMoonDate()))) {"Waning"}
+                else if (Integer.parseInt(dayOfMonth) < (Integer.parseInt(getFullMoonDate()))) {"Waxing"}
+                else{"Plain Moon"}
+
+            return moonString
+        }
+        private fun getFullMoonDate(): String {
+            val d = Date()
+            val sdf0 = SimpleDateFormat("yyyy MMMM")
+            val yearMonth: String = sdf0.format(d)
+            val fullMoonDate = when(yearMonth){
+                "2022 January" -> "17"
+                "2022 February" -> "16"
+                "2022 March" -> "18"
+                "2022 April" -> "16"
+                "2022 May" -> "16"
+                "2022 June" -> "14"
+                "2022 July" -> "13"
+                "2022 August" -> "11"
+                "2022 September" -> "10"
+                "2022 October" -> "9"
+                "2022 November" -> "8"
+                "2022 December" -> "7"
+                "2023 January" -> "6"
+                "2023 February" -> "5"
+                "2023 March" -> "7"
+                "2023 April" -> "5"
+                "2023 May" -> "5"
+                "2023 June" -> "3"
+                "2023 July" -> "3"
+                "2023 August" -> "1"
+                "2023 September" -> "29"
+                "2023 October" -> "28"
+                "2023 November" -> "27"
+                "2023 December" -> "26"
+                "2024 January" -> "25"
+                "2024 February" -> "24"
+                "2024 March" -> "25"
+                "2024 April" -> "23"
+                "2024 May" -> "23"
+                "2024 June" -> "21"
+                "2024 July" -> "21"
+                "2024 August" -> "19"
+                "2024 September" -> "17"
+                "2024 October" -> "17"
+                "2024 November" -> "15"
+                "2024 December" -> "15"
+                "2025 January" -> "13"
+                "2025 February" -> "12"
+                "2025 March" -> "13"
+                "2025 April" -> "12"
+                "2025 May" -> "12"
+                "2025 June" -> "11"
+                "2025 July" -> "10"
+                "2025 August" -> "9"
+                "2025 September" -> "7"
+                "2025 October" -> "6"
+                "2025 November" -> "5"
+                "2025 December" -> "4"
+                "2026 January" -> "3"
+                "2026 February" -> "1"
+                "2026 March" -> "3"
+                "2026 April" -> "1"
+                "2026 May" -> "1"
+                "2026 June" -> "29"
+                "2026 July" -> "29"
+                "2026 August" -> "27"
+                "2026 September" -> "26"
+                "2026 October" -> "25"
+                "2026 November" -> "24"
+                "2026 December" -> "23"
+                "2027 January" -> "22"
+                "2027 February" -> "20"
+                "2027 March" -> "22"
+                "2027 April" -> "20"
+                "2027 May" -> "20"
+                "2027 June" -> "18"
+                "2027 July" -> "18"
+                "2027 August" -> "17"
+                "2027 September" -> "15"
+                "2027 October" -> "15"
+                "2027 November" -> "13"
+                "2027 December" -> "13"
+                "2028 January" -> "11"
+                "2028 February" -> "10"
+                "2028 March" -> "10"
+                "2028 April" -> "9"
+                "2028 May" -> "8"
+                "2028 June" -> "6"
+                "2028 July" -> "6"
+                "2028 August" -> "5"
+                "2028 September" -> "3"
+                "2028 October" -> "3"
+                "2028 November" -> "2"
+                "2028 December" -> "1"
+                else -> "1"
+            }
+            return fullMoonDate
+        }
+
+        private fun getnewMoonDate(): String {
+            val d = Date()
+            val sdf0 = SimpleDateFormat("yyyy MMMM")
+            val yearMonth: String = sdf0.format(d)
+            val newMoonDate = when(yearMonth){
+                "2022 January" -> 2
+                "2022 February" -> 1 //It's Actually Jan31
+                "2022 March" -> 2 //March31
+                "2022 April" -> 30
+                "2022 May" -> 30
+                "2022 June" -> 28
+                "2022 July" -> 28
+                "2022 August" -> 27
+                "2022 September" -> 25
+                "2022 October" -> 25
+                "2022 November" -> 23
+                "2022 December" -> 23
+
+                "2023 January" -> 21
+                "2023 February" -> 19
+                "2023 March" -> 21
+                "2023 April" -> 19
+                "2023 May" -> 19
+                "2023 June" -> 17
+                "2023 July" -> 17
+                "2023 August" -> 16
+                "2023 September" -> 14
+                "2023 October" -> 14
+                "2023 November" -> 13
+                "2023 December" -> 12
+
+                "2024 January" -> 11
+                "2024 February" -> 9
+                "2024 March" -> 10
+                "2024 April" -> 8
+                "2024 May" -> 7
+                "2024 June" -> 6
+                "2024 July" -> 5
+                "2024 August" -> 4
+                "2024 September" -> 2
+                "2024 October" -> 2
+                "2024 November" -> 1 //Nov 30
+                "2024 December" -> 30
+
+                "2025 January" -> 29
+                "2025 February" -> 27
+                "2025 March" -> 29
+                "2025 April" -> 27
+                "2025 May" -> 26
+                "2025 June" -> 25
+                "2025 July" -> 24
+                "2025 August" -> 22
+                "2025 September" -> 21
+                "2025 October" -> 21
+                "2025 November" -> 19
+                "2025 December" -> 19
+
+                "2026 January" -> 18
+                "2026 February" -> 17
+                "2026 March" -> 18
+                "2026 April" -> 17
+                "2026 May" -> 16
+                "2026 June" -> 14
+                "2026 July" -> 14
+                "2026 August" -> 12
+                "2026 September" -> 10
+                "2026 October" -> 10
+                "2026 November" -> 8
+                "2026 December" -> 8
+
+                "2027 January" -> 7
+                "2027 February" -> 6
+                "2027 March" -> 8
+                "2027 April" -> 6
+                "2027 May" -> 6
+                "2027 June" -> 4
+                "2027 July" -> 3
+                "2027 August" -> 2
+                "2027 September" -> 29
+                "2027 October" -> 29
+                "2027 November" -> 27
+                "2027 December" -> 27
+
+                "2028 January" -> 26
+                "2028 February" -> 25
+                "2028 March" -> 25
+                "2028 April" -> 24
+                "2028 May" -> 24
+                "2028 June" -> 22
+                "2028 July" -> 21
+                "2028 August" -> 20
+                "2028 September" -> 18
+                "2028 October" -> 17
+                "2028 November" -> 16
+                "2028 December" -> 15
+                else -> 1
+            }
+            return newMoonDate.toString()
+        }
+
 
 
         private fun initializeBackground() {
@@ -727,6 +939,9 @@ class MyWatchFace : CanvasWatchFaceService() {
             drawStepsFace(canvas)
             drawDaysFace(canvas)
             drawMonthsFace(canvas)
+            drawMonth(canvas, bounds)
+            drawDay(canvas, bounds)
+            drawMoon(canvas, bounds)
             initGrayBackgroundBitmap()
             changeWandColor()
 
@@ -1304,7 +1519,7 @@ class MyWatchFace : CanvasWatchFaceService() {
             }
 
             if (mAmbient) {
-                drawable = R.drawable.ambientmodecalendar
+                drawable = R.drawable.digitalwatchstar
             }
 
             val bitmap = BitmapFactory.decodeResource(applicationContext.resources, drawable)
@@ -1319,6 +1534,121 @@ class MyWatchFace : CanvasWatchFaceService() {
                 null
             )
         }
+
+        private fun drawMonth(canvas: Canvas, bounds: Rect) {
+            val sdf = SimpleDateFormat("EEE")
+            val sdf1 = SimpleDateFormat("EEEE")
+            val sdf2 = SimpleDateFormat("MMM")
+            val sdf3 = SimpleDateFormat("d")
+            val sdf4 = SimpleDateFormat("yyyy")
+            val sdf5 = SimpleDateFormat("MMMM d yyyy")
+            val d = Date()
+            val dayOfTheWeek: String = sdf.format(d)
+            val dayOfTheWeekLong: String = sdf1.format(d)
+            val month: String = sdf2.format(d)
+            val dayOfMonth: String = sdf3.format(d)
+            val year4digits: String = sdf4.format(d)
+            val fullDateSpaces: String = sdf5.format(d)
+            var drawable : Int = when (month){
+                        "Jan" ->R.drawable.jan
+                "Feb" ->R.drawable.feb
+                "Mar" ->R.drawable.mar
+                "Apr" ->R.drawable.apr
+                "May" ->R.drawable.may
+                "Jun" ->R.drawable.jun
+                "Jul" ->R.drawable.jul
+                "Aug" ->R.drawable.aug
+                "Sep" ->R.drawable.sep
+                "Oct" ->R.drawable.oct
+                "Nov" ->R.drawable.nov
+                "Dec" ->R.drawable.dec
+
+               else -> R.drawable.jan}
+
+if (mAmbient) {
+    val bitmap = BitmapFactory.decodeResource(applicationContext.resources, drawable)
+
+    val src = Rect(0, 0, bitmap.height, bitmap.width)
+    val dst = Rect(bounds.left, bounds.top, bounds.right, bounds.bottom)
+
+    canvas.drawBitmap(
+        bitmap,
+        src,
+        dst,
+        null
+    )
+}else{}
+            }
+
+
+        private fun drawDay(canvas: Canvas, bounds: Rect) {
+            val sdf = SimpleDateFormat("EEE")
+            val sdf1 = SimpleDateFormat("EEEE")
+            val sdf2 = SimpleDateFormat("MMM")
+            val sdf3 = SimpleDateFormat("d")
+            val sdf4 = SimpleDateFormat("yyyy")
+            val sdf5 = SimpleDateFormat("MMMM d yyyy")
+            val d = Date()
+            val dayOfTheWeek: String = sdf.format(d)
+            val dayOfTheWeekLong: String = sdf1.format(d)
+            val month: String = sdf2.format(d)
+            val dayOfMonth: String = sdf3.format(d)
+            val year4digits: String = sdf4.format(d)
+            val fullDateSpaces: String = sdf5.format(d)
+            var drawable : Int = when (dayOfTheWeek){
+                "Mon" ->R.drawable.mon
+                "Tues" ->R.drawable.tue
+                "Wed" ->R.drawable.wed
+                "Thu" ->R.drawable.thu
+                "Fri" ->R.drawable.fri
+                "Sat" ->R.drawable.sat
+                "Sun" ->R.drawable.sun
+
+
+                else -> R.drawable.jan}
+
+            if (mAmbient) {
+                val bitmap = BitmapFactory.decodeResource(applicationContext.resources, drawable)
+
+                val src = Rect(0, 0, bitmap.height, bitmap.width)
+                val dst = Rect(bounds.left, bounds.top, bounds.right, bounds.bottom)
+
+                canvas.drawBitmap(
+                    bitmap,
+                    src,
+                    dst,
+                    null
+                )
+            }else{}
+        }
+
+        private fun drawMoon(canvas: Canvas, bounds: Rect) {
+
+            var drawable : Int = when (getMoonPhase()){
+                "Full Moon" ->R.drawable.fullpng
+                "Waxing" ->R.drawable.wax
+                "Waning" ->R.drawable.wan
+                "New Moon" ->R.drawable.newmoontiny
+
+
+
+                else -> R.drawable.wax}
+
+            if (mAmbient) {
+                val bitmap = BitmapFactory.decodeResource(applicationContext.resources, drawable)
+
+                val src = Rect(0, 0, bitmap.height, bitmap.width)
+                val dst = Rect(bounds.left, bounds.top, bounds.right, bounds.bottom)
+
+                canvas.drawBitmap(
+                    bitmap,
+                    src,
+                    dst,
+                    null
+                )
+            }else{}
+        }
+
 
         private fun drawBackground(canvas: Canvas) {
 
